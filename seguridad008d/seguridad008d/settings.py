2310888594f7308f9b96909da7e5b57f2567d917
dar-rojas <dar.rojas@duocuc.cl>
2023-11-27 03:27:00 -0300

    cambio ruta de static

--- seguridad008d/seguridad008d/settings.py
+++ seguridad008d/seguridad008d/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../reclamos/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
